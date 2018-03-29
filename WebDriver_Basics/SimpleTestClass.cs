c516cba797b69298a840b6cbfebf48c0b37d61f9
Jeff Rowell <Jeff Rowell@DESKTOP-BOBAT7O>
2018-03-28 20:53:03 -0600

    Added web driver proxy for google chrome

---
+++ WebDriver_Basics/SimpleTestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PageObjects
+{
+    [TestFixture]
+    public class SimpleTestClass
+    {
+        //Chrome's proxy driver executable is in a folder already
+        //on the host system's PATH environment variable.
+        private IWebDriver _driver = new ChromeDriver();
+
+        [Test]
+        public void TestSiteHeaderIsOnHomePage()
+        {
+            _driver.Navigate().GoToUrl("http://saucelabs.com");
+            IWebElement header = _driver.FindElement(By.Id("site-header"));
+            Assert.True(header.Displayed);
+            _driver.Close();
+        }
+
+        [Test]
+        public void TestTitleIsGoogle()
+        {
+            //Navigate to basic Google page and get title
+            _driver.Navigate().GoToUrl("http://Google.com");
+            string expectedPageTitle = _driver.Title;
+            string actualPageTitle = "Google";
+            Assert.AreEqual(expectedPageTitle, actualPageTitle);
+            _driver.Close();
+        }
+    }
+}
